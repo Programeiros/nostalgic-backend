4c4e1979f5b5caa90c07b0e3bf45dc2870cd37df
Carlos Alberto dos Santos Junior <carlosjunior026@hotmail.com>
2022-04-30 19:38:11 -0300

    chore: adds the validation for the obrigatory methods

--- src/controller/gameController.js
+++ src/controller/gameController.js
@@ -1,4 +1,4 @@
-const utils = require('../utils/gameUtils');
+const utils = require("../utils/gameUtils");
 
 class GameController {
   constructor(app, route) {
@@ -18,7 +18,7 @@ class GameController {
       res.send(utils.getgames());
     });
 
-    this.app.get(this.route + '/:id?', (req, res) => {
+    this.app.get(this.route + "/:id?", (req, res) => {
       res.send(utils.getgameByid(req.params.id));
     });
   }
@@ -26,6 +26,14 @@ class GameController {
   post() {
     this.app.post(this.route, (req, res) => {
       try {
+        this.verify_string(req.body.name, "name");
+        this.verify_string(req.body.description, "description", 500);
+        this.verify_string(req.body.developer, "developer", 50);
+        this.verify_string(req.body.launchdate, "launchdate");
+        this.verify_string(req.body.platforms, "platforms", 50);
+        this.verify_string(req.body.genre, "genre", 50);
+        this.verify_string(req.body.mode, "mode", 100);
+
         const nesgame = utils.setGame(req.body);
 
         res.send(nesgame);
@@ -39,8 +47,16 @@ class GameController {
   }
 
   put() {
-    this.app.put(this.route + '/:id?', (req, res) => {
+    this.app.put(this.route + "/:id?", (req, res) => {
       try {
+        this.verify_string(req.body.name, "name");
+        this.verify_string(req.body.description, "description", 500);
+        this.verify_string(req.body.developer, "developer", 50);
+        this.verify_string(req.body.launchdate, "launchdate");
+        this.verify_string(req.body.platforms, "platforms", 50);
+        this.verify_string(req.body.genre, "genre", 50);
+        this.verify_string(req.body.mode, "mode", 100);
+
         const nesgame = utils.updateGame(req.params.id, req.body);
 
         res.send(nesgame);
@@ -54,7 +70,7 @@ class GameController {
   }
 
   delete() {
-    this.app.delete(this.route + '/:id?', (req, res) => {
+    this.app.delete(this.route + "/:id?", (req, res) => {
       try {
         const nesgame = utils.deleteGame(req.params.id);
         res.send(nesgame);
@@ -65,6 +81,28 @@ class GameController {
         });
       }
     });
+  }
+
+  verify_string(string, string_name, max_length = 80) {
+    let e;
+
+    if (!string || string == "") {
+      console.log(string);
+      e = new Error(`Campo: ${string_name} é  obrigatório!`);
+      throw e;
+    }
+
+    if (typeof string !== "string") {
+      e = new Error(`Campo : ${string_name} precisa ser uma string`);
+      throw e;
+    }
+
+    if (string.length > max_length) {
+      e = new Error(
+        `O Campo: ${string_name} ultrapassou os ${max_length} caracteres!`,
+      );
+      throw e;
+    }
   }
 }
 

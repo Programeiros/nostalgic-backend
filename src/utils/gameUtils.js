99e94d3b9346704de0c7c816e3b152115856aee5
Carlos Alberto dos Santos Junior <carlosjunior026@hotmail.com>
2022-05-01 20:05:20 -0300

    chore: only saves predefined params

--- src/utils/gameUtils.js
+++ src/utils/gameUtils.js
@@ -6,6 +6,17 @@ const sequence = {
 };
 
 const games = {};
+const game = {
+  id: 0,
+  name: "",
+  description: "",
+  developer: "",
+  launchdate: "",
+  platforms: "",
+  genre: "",
+  mode: "",
+  image: "",
+};
 
 function getgames() {
   try {
@@ -29,14 +40,36 @@ function getgameByid(id) {
   }
 }
 
-function setGame(game) {
-  game.id = sequence.id;
-  games[game.id] = game;
-  return game;
+function setGame(value) {
+  value.id = sequence.id;
+
+  game.id = value.id;
+  game.name = value.name;
+  game.description = value.description;
+  game.developer = value.developer;
+  game.genre = value.genre;
+  game.launchdate = value.launchdate;
+  game.mode = value.mode;
+  game.image = value.image;
+  game.platforms = value.platforms;
+
+  games[value.id] = game;
+
+  return games[game.id];
 }
 
 function updateGame(id, newdata) {
-  games[id] = newdata;
+  game.id = newdata.id;
+  game.name = newdata.name;
+  game.description = newdata.description;
+  game.developer = newdata.developer;
+  game.genre = newdata.genre;
+  game.launchdate = newdata.launchdate;
+  game.mode = newdata.mode;
+  game.image = newdata.image;
+  game.platforms = newdata.platforms;
+
+  games[id] = game;
 
   return games[id];
 }

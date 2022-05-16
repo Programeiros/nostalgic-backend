5216940964ce723723e261ee678a46e9c68b4668
Junior <magnojunior319@gmail.com>
2022-05-16 17:14:36 -0400

    added post, put and delete methods

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000;
 const GameController = require("./controller/gameController");
 const game = new GameController(app, "/api/games");
-const db = require('./queries')
+const db = require('./queries');
+const { getgames } = require("./utils/gameUtils");
 
 app.use(bodyParser.json())
 app.use(
@@ -22,8 +23,12 @@ app.get("/", (req, res) => {
   res.json({info:'Aplicação funcionando!'});
 });
 
+
 app.get('/games', db.getGames)
 app.get('/games/:id', db.getGamesById)
+app.post('/games', db.newGame)
+app.put('/games/:id', db.updateGame)
+app.delete('/games/:id', db.deleteGame)
 
 game.start();
 
--- src/queries.js
+++ src/queries.js
@@ -7,18 +7,18 @@ const pool = new Pool({
     port: 5433
 });
 
-const getGames = (require, response) => {
-    pool.query('SELECT * FROM "Games" ORDER BY "Id" ASC', (error, results) => {
+const getGames = (request, response) => {
+    pool.query('SELECT * FROM games ORDER BY id ASC', (error, results) => {
         if (error) {
             throw error
         }
         response.status(200).json(results.rows)
     })
 }
-const getGamesById = (require, response) => {
+const getGamesById = (request, response) => {
     const id = parseInt(request.params.id)
 
-    pool.query('SELECT * FROM "Games" WHERE "Id" = $1', [id], (error, results) => {
+    pool.query('SELECT * FROM games WHERE id = $1', [id], (error, results) => {
         if (error) {
             throw error
         }
@@ -26,8 +26,48 @@ const getGamesById = (require, response) => {
     })
 }
 
+const newGame = (request, response) => {
+    const { name, description, developer, launchdate, platforms, genre, mode, image } = request.body
+  
+    pool.query('INSERT INTO games (name, description, developer, launchdate, platforms, genre, mode, image) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [name, description, developer, launchdate, platforms, genre, mode, image], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).send(`Game added with ID: ${result.insertId}`)
+    })
+  }
+  
+  const updateGame = (request, response) => {
+    const id = parseInt(request.params.id)
+    const { name, description, developer, launchdate, platforms, genre, mode, image } = request.body
+  
+    pool.query(
+      'UPDATE games SET name = $1, description = $2, developer = $3, launchdate = $4, platforms = $5, genre = $6, mode = $7, images = $8 WHERE id = $9',
+      [name, description, developer, launchdate, platforms, genre, mode, image],
+      (error, results) => {
+        if (error) {
+          throw error
+        }
+        response.status(200).send(`Game modified with ID: ${id}`)
+      }
+    )
+  }
+  
+  const deleteGame = (request, response) => {
+    const id = parseInt(request.params.id)
+  
+    pool.query('DELETE FROM games WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`Game deleted with ID: ${id}`)
+    })
+  }
 
 module.exports = {
     getGames,
-    getGamesById
+    getGamesById,
+    newGame,
+    updateGame,
+    deleteGame
 }
